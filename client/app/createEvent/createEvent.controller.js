2a2ea029b9d0603879bfa5ee54b2522efe255172
Justin Sung and Katie Peters <all+justin+katiejoypeters@gmail.com>
2015-01-30 12:27:15 -0500

    createEvent page inputs

--- client/app/createEvent/createEvent.controller.js
+++ client/app/createEvent/createEvent.controller.js
@@ -5,4 +5,10 @@ angular.module('doodleplusApp')
     $scope.message = function(){
       console.log('hi');
     }
+    $scope.invitedEmails = [];
+    $scope.addEmail = function(){
+      if ($scope.emailToAdd){
+        $scope.invitedEmails.push($scope.emailToAdd);
+      }
+    }
   });
