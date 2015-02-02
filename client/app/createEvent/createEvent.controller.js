b630d11f1bd995621a3686bde7cec74289e62ef5
Jordan Landau <jlandau@uoregon.edu>
2015-02-02 18:30:40 -0500

    end of day 2/2/2015

--- client/app/createEvent/createEvent.controller.js
+++ client/app/createEvent/createEvent.controller.js
@@ -24,21 +24,19 @@ angular.module('doodleplusApp')
       label: '1 Day',
       timeIncrement: 86400000
     }];
+
     $scope.date = {};
     $scope.dateToggle = {value: true};
     $scope.dayTimes = [];
 
     $scope.timeOptions.times = [];
 
-    $scope.toggleDate = function()
-    {
-      $scope.dateToggle.value = !$scope.dateToggle.value;
-    }
+    //var hoursTable = angular.element(document.getElementById("hours"));
 
-    $scope.dayView = function()
+    $scope.changeBlocks = function()
     {
       var increment = $scope.selected ? $scope.selected.timeIncrement : 900000;
-      var currentTime = $scope.date.value.getTime();
+      var currentTime = $scope.date.value ?  $scope.date.value.getTime() : -68400000;
       var today = currentTime;
 
       $scope.dayTimes = [];
@@ -48,8 +46,57 @@ angular.module('doodleplusApp')
         $scope.dayTimes.push(new Date(currentTime));
         currentTime += increment;
       }
+    }
+
+//ng-click with $event
+
+    $scope.changeBlocks();
+
+    $scope.$watch('selected', function(newValue, oldValue) {
+      if(newValue != oldValue)
+      {
+        $scope.changeBlocks();
+      }
+    });
+
+    $scope.toggleDate = function()
+    {
+      //$scope.dateToggle.value = !$scope.dateToggle.value;
+      angular.element("#times").toggleClass("ng-hide");
+      angular.element("#days").toggleClass("ng-hide");
+    }
+
+    ceCtrl.isMouseDown = {value:false};
+
+
+    $scope.dayView = function()
+    {
       
       $scope.toggleDate();
+
+      //debugger;
+
+      var hoursTable = angular.element("#hours tr");
+
+      hoursTable
+        .mousedown(function(ele){
+          ceCtrl.isMouseDown.value = true;
+          angular.element(ele.target).toggleClass("success");
+          return false;
+        })
+        .mouseover(function(ele){
+          if(ceCtrl.isMouseDown.value) {
+            angular.element(ele.target).toggleClass("success");
+          }
+        })
+        .bind("selectstart", function(){
+          return false;
+        });
+
+      angular.element(document)
+        .mouseup(function() {
+          ceCtrl.isMouseDown.value = false;
+        });      
     }
 
 
