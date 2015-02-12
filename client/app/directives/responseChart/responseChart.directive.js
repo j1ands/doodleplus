1f6cd79c0c1eb52b3be2c3dfd82bbca54d0425e2
Andrew Watt <awatt11215@gmail.com>
2015-02-12 18:25:09 -0500

    d3 chart basically responsive now, needs styling

--- client/app/directives/responseChart/responseChart.directive.js
+++ client/app/directives/responseChart/responseChart.directive.js
@@ -1,60 +1,54 @@
 'use strict';
 
 angular.module('doodleplusApp')
-.directive('responseChart', ['d3Service', '$stateParams', 'storeEvent', 'Time', function(d3Service, $stateParams, storeEvent, Time) {
+.directive('responseChart', ['d3Service', '$stateParams', 'storeEvent', 'Time', 'responseStatus', function(d3Service, $stateParams, storeEvent, Time, responseStatus) {
 	return {
 		restrict: 'EA',
 		scope: {
 			onRectClick: '&'
 		},
 		link: function(scope, element, attrs) {
+
+			// scope.responseStatus = responseStatus;
+			console.log(responseStatus)
+
 			d3Service.d3().then(function(d3) {
 
-				// var data = [{"start":"2015-08-18 18:00:00","stop":"2015-08-18 20:00:00", "user": "Biff", "status": "Yes"},{"start":"2015-08-21 06:00:00","stop":"2015-08-21 10:00:00", "user": "Ringo", "status": "If Need Be"},{"start":"2015-08-23 18:00:00","stop":"2015-08-23 23:00:00", "user": "Prince", "status": "Maybe"},{"start":"2015-08-23 20:21:00","stop":"2015-08-23 21:21:00", "user": "Charles Barkley", "status": "Yes"},{"start":"2015-08-25 17:01:00","stop":"2015-08-25 22:01:00", "user": "Abe Vigoda", "status": "If Need Be"},{"start":"2015-08-26 17:23:00","stop":"2015-08-26 23:23:00", "user": "Janet Reno", "status": "Unable"},{"start":"2015-08-26 17:52:00","stop":"2015-08-26 23:52:00", "user": "George Costanza", "status": "Yes"},{"start":"2015-08-26 21:01:00","stop":"2015-08-26 23:01:00", "user": "50 Cent", "status": "Unable"},{"start":"2015-08-27 11:23:00","stop":"2015-08-27 23:23:00", "user": "Vladimir Putin", "status": "If Need Be"}];
-				
-				// renderChart(data);
-
 				var days;
-				
-
-
 				var eventID = $stateParams.event_id;
 
-				var allTimes = [];
-
-				storeEvent.getEvent(eventID, function() {
-					console.log(storeEvent.event)
+				storeEvent.getEvent(eventID, null, function() {
 					var event = storeEvent.event;
 					var times = storeEvent.event.times;
 					Time.organizeByDay(times);
 					days = Time.days;
 					days.forEach(function(day){
-						var respondents = [];
+						var allRespondents = [];
 						var allTimes = [];
-						day.times.sort(function(a,b){
+
+						day.times.sort(function(a,b){ 		//sort event times
 							var timeA = a.time;
 							var timeB = b.time;
 							return (timeA < timeB) ? -1 : (timeA > timeB) ? 1 : 0;
 						});
-
-						day.times.forEach(function(time){
+						day.times.forEach(function(time){	//extract all UUIDs and times to reduce
 							for (var i=0; i<time.responses.length; i++){
-								respondents.push(time.responses[i].UUID);
+								allRespondents.push(time.responses[i].UUID);
 								allTimes.push(time.time);
 							};		
-							time.responses.sort(function(a,b){
+							time.responses.sort(function(a,b){  //sort responses by status
 								var textA = a.status.toUpperCase();
 								var textB = b.status.toUpperCase();
 								return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
 							});
 						})
-						day.respondents = respondents.reduce(function(accum, current) {
+						day.allRespondents = allRespondents.reduce(function(accum, current) {  //reduce respondent UUIDs
 							if (accum.indexOf(current) < 0) {
 								accum.push(current);
 							}
 							return accum;
 						}, []);
-						day.allTimes = allTimes.reduce(function(accum, current) {
+						day.allTimes = allTimes.reduce(function(accum, current) {  //reduce times
 							if (accum.indexOf(current) < 0) {
 								accum.push(current);
 							}
@@ -63,46 +57,40 @@ angular.module('doodleplusApp')
 
 					})
 
+renderChart(days[0])
 
-					renderChart(days[0])
+});
 
-			});
+function renderChart (dayData){
 
+	console.log(dayData)
 
-			function renderChart (dayData){
+	var hour = d3.time.format("%H:%M")
 
+	var dayDataFormatted = dayData.allTimes.map(function(day){
+		return hour(new Date(Number(day)));
+	})
 
-				var hour = d3.time.format("%H:%M")
-				
-				var dayDataFormatted = dayData.allTimes.map(function(day){
-					return hour(new Date(Number(day)));
-				})
+	var numResponses = dayData.allRespondents.length,
 
+	responseData = [];
 
+	console.log(dayData)
 
+	dayData.times.forEach(function(time){
+		for (var i=0; i<time.responses.length; i++){
+			time.responses[i].index = i;
+			time.responses[i].time = time.time;
+			time.responses[i].allTimesIndex = dayData.allTimes.indexOf(time.responses[i].time);
+			responseData.push(time.responses[i]);
+		}
+	})
 
-				var numResponses = dayData.respondents.length,
-				
-				responseData = [];
-
-
-				dayData.times.forEach(function(time){
-					for (var i=0; i<time.responses.length; i++){
-						time.responses[i].index = i;
-						time.responses[i].time = time.time;
-						time.responses[i].allTimesIndex = dayData.allTimes.indexOf(time.responses[i].time);
-						responseData.push(time.responses[i]);
-					}
-				})
-
-
-				var m = {top: 40, right: 20, bottom: 20, left: 60},
-				frameWidth = Number((window.innerWidth*.5).toFixed(2)),
-				frameHeight = Number((window.innerHeight*.6).toFixed(2)),
-				// frameWidth = 800,
-				// frameHeight = 800,
-				barSize = frameWidth/8
-				// barSize = frameWidth/dayData.respondents.length;
+	var m = {top: 40, right: 20, bottom: 20, left: 60},
+	frameWidth = Number((window.innerWidth*.6).toFixed(2)),
+	frameHeight = Number((window.innerHeight*.7).toFixed(2)),
+	barSize = frameWidth/8
+				// barSize = frameWidth/dayData.allRespondents.length;
 
 				var yRange = function(){
 					var range = [];
@@ -114,9 +102,8 @@ angular.module('doodleplusApp')
 				
 				var colorCalibration = ['#6CF2A4', '#F2B37C','#7CD1F2']
 
-
 				var svg = d3.select('#chart').append("div")
-				// .attr("class","d3-container container")
+				.attr("class","d3-container container")
 				.selectAll("svg").data(d3.range(1))
 				.enter().append("svg")
 				.attr("id","viz")
@@ -126,25 +113,23 @@ angular.module('doodleplusApp')
 				.attr('transform', 'translate(' + m.left + ', ' + m.top + ')');
 
 
+				function initCalibration(){
+					d3.select('[role="calibration"]').select('svg')
+					.selectAll('rect').data(colorCalibration).enter()
+					.append('rect')
+					.attr('width',20)
+					.attr('height',20)
+					.attr('x',function(d,i){
+						return i*20;
+					})
+					.attr('fill',function(d){
+						return d;
+					});
+				}
 
-				 function initCalibration(){
-				    d3.select('[role="calibration"]').select('svg')
-				      .selectAll('rect').data(colorCalibration).enter()
-				    .append('rect')
-				      .attr('width',20)
-				      .attr('height',20)
-				      .attr('x',function(d,i){
-				        return i*20;
-				      })
-				      .attr('fill',function(d){
-				        return d;
-				      });
-				  }
+				initCalibration();			
 
-				initCalibration();
-
-				function viewBars (data) {
-					
+				function viewBars (dayData) {
 
 					/* add bars to chart */
 					svg.append("g")
@@ -154,6 +139,8 @@ angular.module('doodleplusApp')
 					.enter()
 					.append("rect")
 					.attr("class", function(d){ return d.status + " rect" })
+					.attr("UUID", function(d){ return d.UUID })
+					.attr("time", function(d){ return d.time })
 					.attr("x",function(d) { return d.index*barSize })
 					.attr("y",function(d){ return frameHeight*d.allTimesIndex/dayData.allTimes.length })
 					.attr("width",barSize)
@@ -163,62 +150,40 @@ angular.module('doodleplusApp')
 					.attr("username", function(d){ return d.username})
 					.attr("status", function(d){ return d.status})
 					.on('click', function(d){ 
-						var responses = [];
-						$("rect").each(function() {
-							var mouse = d3.mouse(this)
-							var x = Number(this.getAttribute('x'))
-							var y = Number(this.getAttribute('y'))
-							var username = this.getAttribute('username')
-							var status = this.getAttribute('status')
-							var width = Number(this.getAttribute('width'))
-							var height = Number(this.getAttribute('height'))
 
-							// if(mouse[0] > x && mouse[0] < x + width && mouse[1] > y && mouse[1] < y + height){
-							if(mouse[1] > y && mouse[1] < y + height){
-								responses.push({username: username, status: status});
-								console.log(username + '' + status)
-							}
+						$("rect").each(function() {
+							responseStatus.selectTime.call(this);
 						});
+						$("rect").each(function() {
+							responseStatus.loadStatus.call(this);	
+						});
+					
+						var responses = responseStatus.generateStatus();
+
+						console.log(responses);
+
 						scope.onRectClick({ response: responses });
 					});
+
 
 				//axes, scale and grid/
 
 				var axisWidth = 0,
 				axisHeight = frameHeight,
 				yAxisScale = d3.scale.ordinal()
-					.domain(dayDataFormatted)
-					.range(yRange().concat([""])),
+				.domain(dayDataFormatted)
+				.range(yRange().concat([""])),
 				yAxis = d3.svg.axis()
-					.scale(yAxisScale)
-					.tickValues(dayDataFormatted)
-				// yGrid = d3.svg.axis()
-				// 	.scale(yAxisScale)
-				// 	.orient("left")
-				// 	.ticks(dayDataFormatted.length);
-
+				.scale(yAxisScale)
+				.tickValues(dayDataFormatted)
 
 				svg.append("g")
-					.attr("class", "y left axis")
-					.call(yAxis.orient("left"));	
-
-				// svg.append("g")
-				// 	.attr("class","y grid")
-				// 	.call(yGrid
-				// 		.tickSize(0, 0)
-				// 		.tickFormat(""));
-
-
+				.attr("class", "y left axis")
+				.call(yAxis.orient("left"));	
 			};
 
 			viewBars(dayData);
 		};
-
-
-
-		//then	
 	});
-//link func
 }};
-
 }]);
---
+++ client/app/factories/responseStatus/responseStatus.service.js
@@ -0,0 +1,86 @@
+'use strict';
+
+angular.module('doodleplusApp')
+.factory('responseStatus', function () {
+
+  var responses = [];
+  var numSelected =0;
+  var selectTime =  function() {
+    var mouse = d3.mouse(this)
+    var x = Number(this.getAttribute('x'))
+    var y = Number(this.getAttribute('y'))
+    var width = Number(this.getAttribute('width'))
+    var height = Number(this.getAttribute('height'))
+    
+
+    if(mouse[1] > y && mouse[1] < y + height){
+      if(this.classList.contains('selected')){
+        this.classList.remove('selected');
+        
+      } else {
+        this.classList.add('selected');
+        
+      }  
+    }
+  }
+
+  var loadStatus = function(){
+    var UUID = this.getAttribute('UUID')
+    var username = this.getAttribute('username')
+    var status = this.getAttribute('status')
+    var time = Number(this.getAttribute('time'))
+
+    if (this.classList.contains('selected')){
+      responses.push({username: username, status: status, UUID: UUID, time: time});
+    }
+  }
+
+  var generateStatus = function(){     
+    // var statusArray = ['able','ifneedbe','maybe'];
+    var hash = {};
+    var rows = [];
+    var allRows = [];
+    var displayResponses = [];
+
+    responses.forEach(function(el){
+      rows.push(el.time)
+      if (!hash[el.UUID]){
+        hash[el.UUID] = el;
+        console.log(hash)
+        hash[el.UUID].superStatus = el.status;
+        hash[el.UUID].num = 1;
+        } else {
+          if(el.status > hash[el.UUID].superStatus){
+            hash[el.UUID].superStatus = el.status;
+          }
+          hash[el.UUID].num++;
+        }
+      });
+    
+  allRows = rows.reduce(function(accum, current) {  //reduce times
+                    if (accum.indexOf(current) < 0) {
+                      accum.push(current);
+                    }
+                    return accum;
+                  }, []);
+
+  console.log(allRows.length)
+
+    for (var keys in hash){
+      if (hash[keys].num !== allRows.length){
+        hash[keys].superStatus = 'unable';
+      } else {
+      displayResponses.push(hash[keys]);  
+      }
+    }
+    return displayResponses;
+}
+
+  return {
+    responses: responses,
+    selectTime: selectTime,
+    loadStatus: loadStatus,
+    generateStatus: generateStatus
+  };
+});
+
