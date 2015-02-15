6ad4a7c55f8e9d5dd3293f501cbe67421b7325c7
Jordan Landau <jlandau@uoregon.edu>
2015-02-14 19:21:33 -0500

    removed extraneous console.logs

--- client/app/factories/response/response.service.js
+++ client/app/factories/response/response.service.js
@@ -39,7 +39,6 @@ angular.module('doodleplusApp')
           });
         } else  
         if (time.status) {
-	console.log("HEY USER:", username);
           responses.push({TimeId: time._id, 
                           status: time.status, 
                           username: username,
